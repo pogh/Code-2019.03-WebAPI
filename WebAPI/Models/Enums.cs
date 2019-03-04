5992ed04ffcd1d22ad8a1077018bb40b706d7fb4
pogh <github@spellman.info>
2019-03-04 22:00:20 +0100

    Added Person Persistency Layer

---
+++ WebAPI/Models/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebAPI.Models.Enums
+{
+    public enum ColorEnum
+    {
+        blue = 1,
+        green = 2,
+        purple = 3,
+        red = 4,
+        yellow = 5,
+        turquois = 6,
+        white = 7
+    }
+}
---
+++ WebAPI/Services/IPeople.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Services
+{
+    public interface IPeople
+    {
+        List<Person> EveryOne { get; }
+        Task Add(Person person);
+    }
+}
