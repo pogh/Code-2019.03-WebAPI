8c2ce900e40a5f096b1a7c5876720d736e1b585c
pogh <github@spellman.info>
2019-05-23 16:37:21 +0200

    Got unit test to run

--- xUnitTest/UnitTest1.cs
+++ xUnitTest/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
 namespace xUnitTest
 {
-    public class UnitTest1
+    public class UnitTestGet
     {
         [Fact]
         public void Test1()
         {
+            // Arrange
+            WebAPI.Services.People people = new WebAPI.Services.People();
+            WebAPI.Controllers.ValuesController controller = new WebAPI.Controllers.ValuesController(people);
 
+            // Act
+            ActionResult<string> actionResult = controller.Get();
+
+
+            // Assert
+            Assert.NotEmpty(actionResult.Value);
         }
     }
 }
